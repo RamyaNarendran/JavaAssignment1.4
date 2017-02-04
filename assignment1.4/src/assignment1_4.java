163d5848f68be8a62fa1d2e0ae797d353c66f217
Ramya <nramya1996@gmail.com>
2017-02-04 22:16:14 +0530

    JavaAssignment1.4

---
+++ assignment1.4/src/assignment1_4.java
@@ -0,0 +1,15 @@
+
+public class assignment1_4 {
+public static void main(String args[])
+	{
+		
+		String ch;
+	    int i;
+	    for(i=65; i<91; i++)
+	    {
+	    	ch = new Character((char)i).toString();
+	        System.out.print(i+ "-" + ch + "\n");
+	    }
+			
+	}
+}
